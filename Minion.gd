14be3f78a785958396da470337f18d0dc452f607
rahul <rsv1804@g.rit.edu>
2018-02-24 18:05:07 -0500

    Comments
    added some comments

--- Minion.gd
+++ Minion.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
+# Script of minion
 
 var Health = 3
 var MovementSpeed = 1
 
-var CurrentGridPosition = Vector2(0,0)
 
 func _ready():
 	pass
 
 func _process(delta):
+	
+	# changing the position of minion to the position of the selector
 	self.global_position = get_child(1).SelectedGridPosition
 	pass
--- Selector.gd
+++ Selector.gd
@@ -1,16 +1,19 @@
 extends Sprite
 
+# Script for the selection box of each minion
 
 var MovementCircle=[]
 var MovementSpeed = 2
+onready var map = get_node("/root/World/TileMap")
 
-var SelectedGridPosition =  Vector2(32,32)
+var SelectedGridPosition =  map.map_to_world(Vector2(1,1))
 
 var releaseCounter = 0.5
 
 
 func _process(delta):
 	
+	# timer to detect one press
 	releaseCounter = releaseCounter - delta
 	
 	if(Input.is_key_pressed(KEY_W)):
@@ -21,6 +24,7 @@ func _process(delta):
 		translate(Vector2(-32,0))
 	elif(Input.is_key_pressed(KEY_S)):
 		translate(Vector2(0,32))
+	# On pressing enter the current position is selected
 	elif(Input.is_key_pressed(KEY_ENTER) && releaseCounter < 0):
 		releaseCounter=0.5
 		SelectedGridPosition = self.global_position
