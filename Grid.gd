7c680063e74b1c15ce176a3b2aea203f0ba7ebfd
rahul <rsv1804@g.rit.edu>
2018-02-24 13:54:16 -0500

    Grid
    made a 2d array in grid which isn't used yet

--- Grid.gd
+++ Grid.gd
@@ -3,6 +3,19 @@ extends Node
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var Grid=[]
+var width = 32
+var height = 32
+var i = 0
+var j = 0
 
-
-
+func _ready():
+	
+#	for x in range(width):
+#		Grid[x]=[]
+#		i=i+1
+#		for y in range(height):
+#			j=j+1
+#			Grid[x][y]=0
+			
+	pass
