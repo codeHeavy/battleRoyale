a077b3feb96109cc8b144e4036d4b4e4471504bf
Jishnu Girish <jishnugirish@gmail.com>
2018-02-25 21:58:31 -0500

    death circle

--- GameManager.gd
+++ GameManager.gd
@@ -3,10 +3,12 @@ extends Node
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-
+var turnCounter = 0
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	#node.position = Vector2(64,0)
+	get_child(0).get_child(0).shrink()
 	pass
 
 func _process(delta):
@@ -14,8 +16,11 @@ func _process(delta):
 #	# Update game logic here.
 	get_child(1).TakeTurn(delta)
 	get_child(2).TakeTurn(delta)
-	
 	if (get_child(1).timer > 15 && get_child(2).timer > 15):
 		get_child(1).EndTurn()
 		get_child(2).EndTurn()
+		turnCounter = turnCounter + 1
+	if(turnCounter == 1):
+		get_child(0).get_child(0).shrink()
+		turnCounter = 0
 	pass
--- ViewGrid.gd
+++ ViewGrid.gd
@@ -3,16 +3,27 @@ onready var grid = get_parent()
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var node1
+var node2
+var node3
+var node4
+var crowd
+
+var xStart = -1
+var xEnd = 30
+var yStart = -1
+var yEnd = 17
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	crowd = load("res://crowd.tscn")
 	pass
 func _draw():
 	var LINECOLOR = Color(255,0,0)
 	var LINEWIDTH = 2
 	var window_size = OS.get_window_size()
-	
+	#generateCrowd()
 	for x in range(grid.gridSize.x + 1):
 		var colPos = x * grid.tileSize.x
 		var limit = grid.gridSize.y * grid.tileSize.y
@@ -20,9 +31,38 @@ func _draw():
 		for y in range(grid.gridSize.y + 1):
 			var rowPos = y * grid.tileSize.y
 			var limitR = grid.gridSize.x * grid.tileSize.y
+			#node = crowd.instance()
+			#add_child(node)
+			#node.position = (Vector2(colPos,rowPos))
+			print(rowPos," " , colPos)
 			draw_line(Vector2(0,rowPos),Vector2(limit,rowPos),LINECOLOR,LINEWIDTH)
 	pass
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+func shrink():
+	xStart = xStart + 1;
+	xEnd = xEnd - 1;
+	yStart = yStart + 1;
+	yEnd = yEnd - 1;
+	for x in range(grid.gridSize.x + 1):
+		var colPos = x * grid.tileSize.x
+		var limit = grid.gridSize.y * grid.tileSize.y
+		for y in range(grid.gridSize.y + 1):
+			var rowPos = y * grid.tileSize.y
+			var limitR = grid.gridSize.x * grid.tileSize.y
+			node1 = crowd.instance()
+			node2 = crowd.instance()
+			node3 = crowd.instance()
+			node4 = crowd.instance()
+			add_child(node1)
+			add_child(node2)
+			add_child(node3)
+			add_child(node4)
+			node1.position = (Vector2(xStart * 64, colPos))
+			node2.position = (Vector2((xEnd)* 64, colPos))
+			node3.position = (Vector2(rowPos , yStart * 64))
+			node4.position = (Vector2(colPos, (yEnd) * 64))
+			print(rowPos," " , colPos)
+	pass
